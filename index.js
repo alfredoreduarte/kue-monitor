9ee9c1846fb958d3bff900fcb4e6fbbf2dba03e1
Alfredo Re <alfredoreduarte@gmail.com>
2019-01-23 17:24:11 -0300

    Added basic auth

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const kue = require('kue')
+const express = require('express')
+const basicAuth = require('basic-auth-connect')
 const PORT = process.env.PORT || 5000
+
+const app = express()
 
 kue.createQueue({
 	redis: {
@@ -8,6 +12,7 @@ kue.createQueue({
 		db: process.env.REDIS_DB_NUMBER,
 	}
 })
-
-kue.app.listen(PORT)
+app.use(basicAuth(process.env.AUTH_USERNAME, process.env.AUTH_PASSWORD))
+app.use(kue.app)
+app.listen(PORT)
 console.log("Running on http://127.0.0.1:" + PORT)
